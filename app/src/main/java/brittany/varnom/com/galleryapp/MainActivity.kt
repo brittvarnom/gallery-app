2604b6de070f8677c0ff4681fe0ff5798a83a60e
brittvarnom <brittany.varnom@bbc.co.uk>
2018-02-05 15:23:52 +0000

    initial commit

---
+++ app/src/main/java/brittany/varnom/com/galleryapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package brittany.varnom.com.galleryapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
